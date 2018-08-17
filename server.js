2fd6f49159c66b51d3f274bac26e614b93b78efa
Rajeev Kumar Singh <rajeevs@flock.co>
2018-08-17 09:56:53 +0530

    upgraded mongoose and body-parser versions

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url)
-.then(() => {
+mongoose.connect(dbConfig.url, {
+	useNewUrlParser: true
+}).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...');
